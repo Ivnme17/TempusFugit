01c76dbfd7402cfa011301004162c1f190723290
Iván Martínez Estrada <116352679+Ivnme17@users.noreply.github.com>
2025-02-12 14:13:40 +0100

    Update tempusFugitDb.sql

--- db/tempusFugitDb.sql
+++ db/tempusFugitDb.sql
@@ -1,25 +1,38 @@
-CREATE DATABASE IF NOT EXISTS `tempus-fugit`;/*Creamos la base de datos*/
+CREATE DATABASE IF NOT EXISTS `tempus-fugit`;
+USE `tempus-fugit`;
 
-USE `tempus-fugit`;/*Usamos la base de datos*/
-
-/*CREAMOS LAS TABLAS QUE VAN A FORMAR PARTE DE LA BASE DE DATOS*/
 
 CREATE TABLE Roles (
-    ID_rol INT(11) PRIMARY KEY,
+    ID_rol INT PRIMARY KEY,
     tipo VARCHAR(20)   
-)DEFAULT CHARSET=utf8mb4 COLLATE=utf8_general_ci;
+);
 
+
+CREATE TABLE Usuarios (
+    ID_Usuario INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellidos VARCHAR(100),
+    contrasena VARCHAR(255),
+    ID_rol INT,
+    CONSTRAINT fk_usuarios_idRol FOREIGN KEY (ID_rol) REFERENCES Roles(ID_rol) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
 
 CREATE TABLE Clientes (
-    ID_cliente INT PRIMARY KEY,
-    nombre VARCHAR(50),
-    apellido VARCHAR(100),
-    correo VARCHAR(100),
+    ID_Usuario INT PRIMARY KEY,
     telefono VARCHAR(15),
+    correo VARCHAR(100),
     direccion VARCHAR(255),
-    ID_rol INT(11) ,
-    CONSTRAINT fk_clientes_idRol FOREIGN KEY (ID_rol) REFERENCES Roles(ID_rol) ON DELETE SET NULL ON UPDATE CASCADE
+    IBAN VARCHAR(34),
+    CONSTRAINT fk_clientes_idUsuario FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID_Usuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Empleados (
+    ID_Usuario INT PRIMARY KEY,
+    DNI VARCHAR(20),
+    NSS VARCHAR(20),
+    CONSTRAINT fk_empleados_idUsuario FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID_Usuario) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -29,20 +42,11 @@ CREATE TABLE Relojes (
     modelo VARCHAR(100),
     precio DECIMAL(10, 2),
     tipo ENUM('digital', 'analógico'),
-    disponibilidad BOOLEAN DEFAULT TRUE
+    disponibilidad BOOLEAN DEFAULT TRUE,
+    ID_Usuario INT,
+    CONSTRAINT fk_relojes_idUsuario FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID_Usuario) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
-
-CREATE TABLE Ventas (
-    ID_venta INT AUTO_INCREMENT PRIMARY KEY,
-    ID_cliente INT,
-    ID_reloj INT,
-    fecha_venta DATETIME DEFAULT CURRENT_TIMESTAMP,
-    cantidad INT,
-    total_venta DECIMAL(10, 2),
-    CONSTRAINT fk_ventas_idCliente FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_cliente) ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT fk_ventas_idReloj FOREIGN KEY (ID_reloj) REFERENCES Relojes(ID_reloj) ON DELETE SET NULL ON UPDATE CASCADE
-);
 
 CREATE TABLE Servicios (
     ID_servicio INT AUTO_INCREMENT PRIMARY KEY,
@@ -50,13 +54,25 @@ CREATE TABLE Servicios (
     precio_base DECIMAL(10, 2)
 );
 
+
 CREATE TABLE Ordenes_Servicio (
-    ID_orden INT AUTO_INCREMENT PRIMARY KEY,
-    ID_cliente INT,
+    ID_Usuario INT,
     ID_servicio INT,
-    fecha_orden DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fecha_orden DATETIME,
     estado ENUM('pendiente', 'en progreso', 'completado') DEFAULT 'pendiente',
     costo_total DECIMAL(10, 2),
-    CONSTRAINT fk_ordenes_idCliente FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_cliente) ON DELETE SET NULL  ON UPDATE CASCADE ,
+    PRIMARY KEY(ID_usuario,ID_servicio,fecha_orden),
+    CONSTRAINT fk_ordenes_idUsuario FOREIGN KEY (ID_Usuario) REFERENCES Clientes(ID_Usuario) ON DELETE SET NULL  ON UPDATE CASCADE,
     CONSTRAINT fk_ordenes_idServicio FOREIGN KEY (ID_servicio) REFERENCES Servicios(ID_servicio) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE Gestion (
+    ID_Usuario INT,
+    ID_reloj INT,
+    PRIMARY KEY(ID_usuario,ID_reloj),
+    CONSTRAINT fk_gestion_idUsuarioCli FOREIGN KEY (ID_Usuario) REFERENCES Clientes(ID_Usuario) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_gestion_idUsuarioEmp FOREIGN KEY (ID_Usuario) REFERENCES Empleados(ID_Usuario) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT fk_gestion_idReloj FOREIGN KEY (ID_reloj) REFERENCES Reloj(ID_reloj) ON DELETE SET NULL ON UPDATE CASCADE
 );
