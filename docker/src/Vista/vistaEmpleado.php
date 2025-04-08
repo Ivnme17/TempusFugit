bd7c6e1f2f01cd7a5ab80a9137f9bdff3d397dcf
Iván Martínez Estrada <116352679+Ivnme17@users.noreply.github.com>
2025-04-08 11:00:29 +0200

    Actualizar la ruta de los require_once en vistaEmpleado.php para asegurar el correcto funcionamiento y agregar la lógica de cierre de sesión en cerrarSesion.php.

--- docker/src/Vista/vistaEmpleado.php
+++ docker/src/Vista/vistaEmpleado.php
@@ -1,9 +1,9 @@
 <?php
-require_once './Servicio/Db.php';
-require_once './Modelo/Usuario.php';
+// He tenido que cambiar la ruta de los require_once para que funcionen correctamente
+require_once __DIR__ . '/../Servicio/Db.php';
+require_once __DIR__ . '/../Modelo/Usuario.php';
+
 $clientesObj = Usuario::listarClientes();
-//Si clientesObj es un array, lo asignamos a $clientes, si no, lo convertimos en un array
-// para poder recorrerlo en el foreach.
 
 $relojes = [];
 $conexion = Db::getConexion();
--- docker/src/cerrarSesion.php
+++ docker/src/cerrarSesion.php
@@ -1,1 +1,10 @@
 <!-- fichero que si se encuentra una session activa la elimina y redirige al usuario a la página principal    !>
+<?php
+session_start();
+if (isset($_SESSION["usuario"]) && isset($_SESSION["rol"])){
+    session_destroy(); 
+    header("Location: index.php"); 
+    exit(); 
+} else {
+    echo "No hay sesión activa.";
+}
