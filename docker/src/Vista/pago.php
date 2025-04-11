3d2d23b318ddd5cacaae5016358547920779ea40
Iván Martínez Estrada <116352679+Ivnme17@users.noreply.github.com>
2025-04-11 17:26:59 +0200

    Agregar mensaje de redirección al usuario tras el pago exitoso

--- docker/src/Vista/pago.php
+++ docker/src/Vista/pago.php
@@ -28,6 +28,7 @@ if(isset($_POST['pagar'])) {
     header("refresh:3;url=vistaCliente.php");
     echo "<dialog open style='padding: 30px; border-radius: 20px; border: none; box-shadow: 0 0 20px rgba(0,0,0,0.3); background-color:#00205B;'>
            <p style='color: white; font-size: 18px; margin: 0;'>Pago realizado correctamente!!</p>
+           <pre style=color:white>redirigiendo...</pre>
         </dialog>";
     
     $cesta = $_SESSION['cesta'];
